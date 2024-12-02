f00ef77f30bdb7ce043f410476f05db180f1389f
jor-eldevera <jorell.devera@gmail.com>
2024-12-02 12:02:06 -0800

    Add farm two code

--- src/index.js
+++ src/index.js
@@ -5,42 +5,62 @@ import { Farm } from "./Farm.js";
 const mainButtonBtn = document.getElementById("main-button");
 const bankP = document.getElementById("bank");
 const farmOneBtn = document.getElementById("farm1-button");
+const farmTwoBtn = document.getElementById("farm2-button");
 
 let mainButton = new Button();
 let bank = new Bank();
 
-let farmOne = new Farm("Farm1", 1, 20, 1.2);
+let farmArray = [];
+let farmOne = new Farm("Farm1", 1, 20, 1.5);
+farmArray.push(farmOne);
 farmOneBtn.innerText = "Buy " + farmOne.getName() + ": " + farmOne.getNextFarmCost();
+
+let farmTwo = new Farm("Farm2", 10, 200, 1.5);
+farmArray.push(farmTwo);
+farmTwoBtn.innerText = "Buy " + farmTwo.getName() + ": " + farmTwo.getNextFarmCost();
 
 let mainButtonText = "Add ";
 let mainButtonValue = mainButton.getCurrentValue();
 
 mainButtonBtn.addEventListener("click", (e) => {
     bank.add(mainButtonValue);
-    updateTotalCounterP();
+    updateBankP();
 });
 
 farmOneBtn.addEventListener("click", (e) => {
     if (farmOne.getNextFarmCost() <= bank.getTotal()) {
         bank.subtract(farmOne.getNextFarmCost());
         farmOne.buyFarm();
-        updateTotalCounterP();
+        updateBankP();
         farmOneBtn.innerText = "Buy " + farmOne.getName() + ": " + farmOne.getNextFarmCost();
     }
 });
 
-function updateTotalCounterP() {
+farmTwoBtn.addEventListener("click", (e) => {
+    if (farmTwo.getNextFarmCost() <= bank.getTotal()) {
+        bank.subtract(farmTwo.getNextFarmCost());
+        farmTwo.buyFarm();
+        updateBankP();
+        farmTwoBtn.innerText = "Buy " + farmTwo.getName() + ": " + farmTwo.getNextFarmCost();
+    }
+});
+
+// Updates the text of the bank
+function updateBankP() {
     bankP.innerText = "" + bank.getTotal();
 }
+
+// Loop clock code
 const intervalID = setInterval(harvest, 1000);
 function harvest() {
     let total = 0;
 
-    // farm one
-    total += farmOne.getTotalAmountPerSecond();
+    farmArray.forEach(farm => {
+        total += farm.getTotalAmountPerSecond();
+    });
 
     bank.add(total);
-    updateTotalCounterP();
+    updateBankP();
 }
 
 function endHarvest() {
