4b8f02e129bbe30e9bf939c49d218dd8cc9f74f8
jor-eldevera <jorell.devera@gmail.com>
2024-12-02 00:05:40 -0800

    Add farm button that subtracts from total and increases in price

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import { Bank } from "./Bank.js";
 import { Button } from "./Button.js";
+import { Farm } from "./Farm.js";
 
 const mainButtonBtn = document.getElementById("main-button");
-const totalCounterP = document.getElementById("total-counter");
+const bankP = document.getElementById("bank");
+const farmOneBtn = document.getElementById("farm1-button");
 
 let mainButton = new Button();
 let bank = new Bank();
+
+let farmOne = new Farm("Farm1", 1, 20, 1.2);
+farmOneBtn.innerText = "Buy " + farmOne.getName() + ": " + farmOne.getNextFarmCost();
 
 let mainButtonText = "Add ";
 let mainButtonValue = mainButton.getCurrentValue();
 
 mainButtonBtn.addEventListener("click", (e) => {
     bank.add(mainButtonValue);
-    totalCounterP.innerText = "" + bank.getTotal();
+    updateTotalCounterP();
 });
+
+farmOneBtn.addEventListener("click", (e) => {
+    if (farmOne.getNextFarmCost() <= bank.getTotal()) {
+        bank.subtract(farmOne.getNextFarmCost());
+        farmOne.buyFarm();
+        updateTotalCounterP();
+        farmOneBtn.innerText = "Buy " + farmOne.getName() + ": " + farmOne.getNextFarmCost();
+    }
+});
+
+function updateTotalCounterP() {
+    bankP.innerText = "" + bank.getTotal();
+}
